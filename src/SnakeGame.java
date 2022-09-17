2094412192a73a8eb09a852003dabd97ba698a0d
kishumittal01 <mkishu15@gmail.com>
2022-09-17 17:27:01 +0530

    first commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,7 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();// shortcut to  GameFrame frame = new GameFrame....as it will not be used anywhere else
+
+    }
+}
